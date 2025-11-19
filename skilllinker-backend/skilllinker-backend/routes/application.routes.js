6e4b48bbfe00a2deda449a6fbbdd6a7e5833c4c8
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-19 19:09:19 +0200

    add routes for Application CRUD operations

---
+++ skilllinker-backend/skilllinker-backend/routes/application.routes.js
@@ -0,0 +1,22 @@
+// routes/application.routes.js
+const express = require('express');
+const router = express.Router();
+const ApplicationController = require('../Controllers/application.controller');
+const { validateApplication } = require('../Middlewares/application.middleware');
+
+// Create a new application
+router.post('/', validateApplication, ApplicationController.createApplication);
+
+// Get all applications
+router.get('/', ApplicationController.getAllApplications);
+
+// Get single application
+router.get('/:id', ApplicationController.getApplicationById);
+
+// Update application status
+router.put('/:id/status', ApplicationController.updateApplicationStatus);
+
+// Delete an application
+router.delete('/:id', ApplicationController.deleteApplication);
+
+module.exports = router;
