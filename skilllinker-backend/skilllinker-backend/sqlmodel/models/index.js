986402d67f39a16f49bd6347236d1cf7a2ac7e63
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-19 19:38:32 +0200

    staging

--- skilllinker-backend/skilllinker-backend/sqlmodel/models/index.js
+++ skilllinker-backend/skilllinker-backend/sqlmodel/models/index.js
@@ -8,8 +8,9 @@ const sequelize = require(path.join(__dirname, '..', '..', 'config', 'database')
 const User = require('./User')(sequelize, Sequelize.DataTypes);
 const Job = require('./Job')(sequelize, Sequelize.DataTypes);
 
-// Class-style model
-const Application = require('./Application'); // <-- do NOT call it
+// Class-style models
+const Application = require('./Application');
+const Message = require('./Message')(sequelize, Sequelize.DataTypes); // if function-style
 
 // Associations
 User.hasMany(Application, { foreignKey: 'userId' });
@@ -18,11 +19,21 @@ Application.belongsTo(User, { foreignKey: 'userId' });
 Job.hasMany(Application, { foreignKey: 'jobId' });
 Application.belongsTo(Job, { foreignKey: 'jobId' });
 
+// Messages
+User.hasMany(Message, { foreignKey: 'fromUserId', as: 'sentMessages' });
+User.hasMany(Message, { foreignKey: 'toUserId', as: 'receivedMessages' });
+Message.belongsTo(User, { foreignKey: 'fromUserId', as: 'sender' });
+Message.belongsTo(User, { foreignKey: 'toUserId', as: 'recipient' });
+
+Job.hasMany(Message, { foreignKey: 'jobId' });
+Message.belongsTo(Job, { foreignKey: 'jobId' });
+
 // Export
 module.exports = {
   sequelize,
   Sequelize,
   User,
   Job,
-  Application
+  Application,
+  Message
 };
