a72b96ae59ce8f612328e1e0cd6a1191a8158056
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-19 20:59:49 +0200

    updating collection of the postman

--- skilllinker-backend/skilllinker-backend/routes/users.routes.js
+++ skilllinker-backend/skilllinker-backend/routes/users.routes.js
@@ -8,7 +8,7 @@ router.post('/register', usersController.register);
 router.post('/login', usersController.login);
 
 // Protected routes
-router.get('/', authenticate, usersController.getAll);
+router.get('/', usersController.getAll);
 router.put('/:id', authenticate, usersController.update);
 router.delete('/:id', authenticate, usersController.delete);
 
