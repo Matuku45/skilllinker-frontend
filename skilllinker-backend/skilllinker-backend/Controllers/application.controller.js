b10a7a0f193a44d7db86f5d0e86fa89761e95949
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-19 19:49:02 +0200

    application.controller.js is exporting a class, not a plain object with functions.

--- skilllinker-backend/skilllinker-backend/Controllers/application.controller.js
+++ skilllinker-backend/skilllinker-backend/Controllers/application.controller.js
@@ -1,26 +1,26 @@
 // Controllers/application.controller.js
 const ApplicationService = require('../Services/application.service');
 
-class ApplicationController {
-  static async createApplication(req, res, next) {
+const ApplicationController = {
+  createApplication: async (req, res, next) => {
     try {
       const app = await ApplicationService.createApplication(req.body);
       res.status(201).json(app);
     } catch (error) {
       next(error);
     }
-  }
+  },
 
-  static async getAllApplications(req, res, next) {
+  getAllApplications: async (req, res, next) => {
     try {
       const apps = await ApplicationService.getAllApplications();
       res.status(200).json(apps);
     } catch (error) {
       next(error);
     }
-  }
+  },
 
-  static async getApplicationById(req, res, next) {
+  getApplicationById: async (req, res, next) => {
     try {
       const app = await ApplicationService.getApplicationById(req.params.id);
       if (!app) return res.status(404).json({ message: 'Application not found' });
@@ -28,9 +28,9 @@ class ApplicationController {
     } catch (error) {
       next(error);
     }
-  }
+  },
 
-  static async updateApplicationStatus(req, res, next) {
+  updateApplicationStatus: async (req, res, next) => {
     try {
       const updatedApp = await ApplicationService.updateApplicationStatus(
         req.params.id,
@@ -40,16 +40,16 @@ class ApplicationController {
     } catch (error) {
       next(error);
     }
-  }
+  },
 
-  static async deleteApplication(req, res, next) {
+  deleteApplication: async (req, res, next) => {
     try {
       await ApplicationService.deleteApplication(req.params.id);
       res.status(204).send();
     } catch (error) {
       next(error);
     }
-  }
-}
+  },
+};
 
 module.exports = ApplicationController;
